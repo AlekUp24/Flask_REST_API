8ed9b29d18f0e4525fd7187705dfc86ef61a92aa
AlekUp24 <alekp24@gmail.com>
2024-03-01 19:51:22 +0100

    nowy

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ video_put_args.add_argument('name', type=str, help='Name of the video is require
 video_put_args.add_argument('views', type=int, help='Views of the video is required', required = True)
 video_put_args.add_argument('likes', type=int, help='Likes on the video is required', required = True)
 
-
+#SSS
 
 videos = {}
 
--- test.py
+++ test.py
@@ -4,4 +4,3 @@ BASE = "http://127.0.0.1:5000/"
 
 response = requests.put(BASE + 'video/1', {'likes':10})
 print(response.json())
-
