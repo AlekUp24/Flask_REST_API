b123f322d640c5cc974314f24baf008c4a222681
AlekUp24 <alekp24@gmail.com>
2024-03-11 09:22:44 +0100

    jakas

--- test.py
+++ test.py
@@ -2,7 +2,29 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.put(BASE + 'video/1', {'likes':10 , 'name': 'Hubele', 'views': 1000000})
+data = [{'likes':10 , 'name': 'Hubele', 'views': 1000000},
+        {'likes':78 , 'name': 'How to make rest APIs', 'views': 90000},
+        {'likes':876 , 'name': 'My firts video', 'views': 10},
+        {'likes':13 , 'name': 'Nowy jpeg', 'views': 3}]
+
+
+for i in range(len(data)):
+    response = requests.put(BASE + 'video/' +str(i), data[i])
+    print(response.json())
+
+
+response = requests.delete(BASE + "video/0")
+print(response)
+ 
+input()
+
+response = requests.get(BASE + 'video/2')
+print(response.json())
+
+input()
+
+'''
+response = requests.put(BASE + 'video/3', {'likes':10 , 'name': 'Hubele', 'views': 1000000})
 print(response.json())
 
 input()
@@ -14,3 +36,4 @@ input()
 
 response = requests.get(BASE + 'video/2')
 print(response.json())
+'''
