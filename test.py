3ff1ca592f5a3cffb9e42b22cd749f4fddb679c7
AlekUp24 <alekp24@gmail.com>
2024-03-11 10:23:34 +0100

    db added

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ BASE = "http://127.0.0.1:5000/"
 
 data = [{'likes':10 , 'name': 'Hubele', 'views': 1000000},
         {'likes':78 , 'name': 'How to make rest APIs', 'views': 90000},
-        {'likes':876 , 'name': 'My firts video', 'views': 10},
+        {'likes':87 , 'name': 'My firts video', 'views': 10},
         {'likes':13 , 'name': 'Nowy jpeg', 'views': 3}]
 
 
@@ -12,16 +12,11 @@ for i in range(len(data)):
     response = requests.put(BASE + 'video/' +str(i), data[i])
     print(response.json())
 
-
-response = requests.delete(BASE + "video/0")
-print(response)
- 
 input()
 
-response = requests.get(BASE + 'video/2')
+response = requests.patch(BASE + 'video/2', {'views': 99, 'likes':101})
 print(response.json())
 
-input()
 
 '''
 response = requests.put(BASE + 'video/3', {'likes':10 , 'name': 'Hubele', 'views': 1000000})
